f12d9a8c88df74864aa1db2bdfeb3ebaa1223ca4
Daniel52x <danielcost0705@gmail.com>
2021-11-26 00:36:28 -0300

    Consultar estado de saude por periodo de data

--- src/main/java/ps2/restapidb/EstadoSaudeRepo.java
+++ src/main/java/ps2/restapidb/EstadoSaudeRepo.java
@@ -1,11 +1,18 @@
 package ps2.restapidb;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EstadoSaudeRepo extends CrudRepository<EstadoSaude, Long>{
-    @Query("select e from EstadoSaude e where e.usuario.id = ?1 and 1=1")
+    @Query("select e from EstadoSaude e where e.usuario.id = ?1")
     Optional<List<EstadoSaude>> findByIdUsuario(long idUsuario);
+    
+    @Query("select e from EstadoSaude e where e.usuario.id = ?1 AND DATE(e.dtCriacao) >= ?2 ORDER BY e.dtCriacao ASC")
+    Optional<List<EstadoSaude>> findByIdUsuarioAndInitialDate(long idUsuario, Date initial);
+    
+    @Query("select e from EstadoSaude e where e.usuario.id = ?1 AND (DATE(e.dtCriacao) >= ?2 AND DATE(e.dtCriacao) <= ?3) AND 1=1 ORDER BY e.dtCriacao ASC")
+    Optional<List<EstadoSaude>> findByIdUsuarioAndInitiallEndDate(long idUsuario, Date initial, Date end);
 }
