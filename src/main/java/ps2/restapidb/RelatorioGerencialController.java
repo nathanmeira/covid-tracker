65020820f69411bd1c38ae51a2558a4ea742d1f7
Daniel52x <danielcost0705@gmail.com>
2021-11-28 01:48:24 -0300

    Quantidade diária bem e nao bem

--- src/main/java/ps2/restapidb/RelatorioGerencialController.java
+++ src/main/java/ps2/restapidb/RelatorioGerencialController.java
@@ -26,7 +26,7 @@ public class RelatorioGerencialController {
     private SintomaRepo sintomaRepo;
 
     @GetMapping("/api/relatorios/registros/qtd")
-    public List<Object> getEstadosSaude(
+    public List<Object> getQtdRegistros(
         @RequestParam("start") Optional<String> start,
         @RequestParam("end") Optional<String> end
     ) throws ParseException {      
@@ -45,6 +45,52 @@ public class RelatorioGerencialController {
         }      
       
         Optional<List<Object>> s = this.estadoSaudeRepo.getQtdRegistrosDiario(dateStart, dateEnd);
+        return s.get();
+    }
+    
+    @GetMapping("/api/relatorios/registros/doentes/qtd")
+    public List<Object> getQtdRegistrosNaoMuitoBem(
+        @RequestParam("start") Optional<String> start,
+        @RequestParam("end") Optional<String> end
+    ) throws ParseException {      
+        LocalDateTime now = LocalDateTime.now(); 
+        String dateNowFormat = now.getYear() + "-" + now.getMonthValue()+ "-" + now.getDayOfMonth();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        
+        Date dateStart = formatter.parse("2021-01-01");
+        Date dateEnd = formatter.parse(dateNowFormat);
+        
+        if(start.isPresent() && !end.isPresent()){
+            dateStart = formatter.parse(start.get());
+        } else if(start.isPresent() && end.isPresent()) {
+            dateStart = formatter.parse(start.get());
+            dateEnd = formatter.parse(end.get());
+        }      
+      
+        Optional<List<Object>> s = this.estadoSaudeRepo.getQtdRegistrosByEstadoSaude(dateStart, dateEnd, false);
+        return s.get();
+    }
+    
+    @GetMapping("/api/relatorios/registros/bem/qtd")
+    public List<Object> getQtdRegistrosBem(
+        @RequestParam("start") Optional<String> start,
+        @RequestParam("end") Optional<String> end
+    ) throws ParseException {      
+        LocalDateTime now = LocalDateTime.now(); 
+        String dateNowFormat = now.getYear() + "-" + now.getMonthValue()+ "-" + now.getDayOfMonth();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        
+        Date dateStart = formatter.parse("2021-01-01");
+        Date dateEnd = formatter.parse(dateNowFormat);
+        
+        if(start.isPresent() && !end.isPresent()){
+            dateStart = formatter.parse(start.get());
+        } else if(start.isPresent() && end.isPresent()) {
+            dateStart = formatter.parse(start.get());
+            dateEnd = formatter.parse(end.get());
+        }      
+      
+        Optional<List<Object>> s = this.estadoSaudeRepo.getQtdRegistrosByEstadoSaude(dateStart, dateEnd, true);
         return s.get();
     }
 }
