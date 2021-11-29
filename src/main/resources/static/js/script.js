a7c0a75d3efb5eb23b0444c338209044a646b242
Nathan M <KZ4W//9qu3c5iD2N2k4GH0frg5BjAQ+a5sYvlLo3oac=>
2021-11-28 21:37:24 -0300

    features: authentication, localstorage management, layout fixes and small improvements

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,42 @@
+const userId = localStorage.getItem('userID');
+const userName = localStorage.getItem('username');
+const userType = localStorage.getItem('userType');
+
+function showDropdown() {
+    const dropdownAdmin = document.querySelector('#drop-admin');
+    const dropdownMembro = document.querySelector('#drop-membro');
+
+    if ( userType === 1 || userType === '1') {
+        dropdownAdmin.classList.remove('hide');
+        dropdownMembro.classList.add('hide');
+    } else {
+        dropdownMembro.classList.remove('hide');
+        dropdownAdmin.classList.add('hide');
+    }
+}
+
+function openedSession(){
+    if ((userId && userName && userType) !== null) {
+        window.location.href = "/tracker.html";
+    }
+}
+
+function checkSection(){
+    if (userId == null || userName == null || userType == null) {
+        localStorage.removeItem('userID');
+        localStorage.removeItem('username');
+        localStorage.removeItem('userType');
+        window.location.href = "/index.html";
+    }
+}
+
+function logOut(){
+    localStorage.removeItem('userID');
+    localStorage.removeItem('username');
+    localStorage.removeItem('userType');
+    window.location.href = "/index.html";
+}
+
 const capitalize = (str) => {
     if(typeof str === 'string') {
         return str.replace(/^\w/, c => c.toUpperCase());
@@ -5,4 +44,3 @@ const capitalize = (str) => {
         return '';
     }
 };
-
